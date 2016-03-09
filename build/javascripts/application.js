75ec997619d8056bc4d1a918aa07639b768f6dcd
Rafael Mariano <toruticas@gmail.com>
2016-03-08 22:12:29 -0300

    Version 0.1 created.

--- build/javascripts/application.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  var Run;
-  angular.module('gaperia', ['ngMessages', 'ui.bootstrap', 'ui.router', 'uiGmapgoogle-maps']);
-  Run = function($rootScope, $location) {
-    return $rootScope.localTime = new Date();
-  };
-  Run.$inject = ['$rootScope', '$location'];
-  return angular.module('gaperia').run(Run);
-})();
